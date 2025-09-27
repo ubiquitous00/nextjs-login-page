37ffcf1ba14730892cb2314116eb15cb03e47586
tim <xuegot@hotmail.com>
2025-09-27 14:20:02 +1000

    Fix indentation

--- app/ui/login/login-form.tsx
+++ app/ui/login/login-form.tsx
@@ -22,34 +22,34 @@ export default function LoginForm() {
 	return (
 		<form action={formAction}>
 			<div className="mb-4">
-			<label className="block mb-1 font-medium" htmlFor="username">
-				Username
-			</label>
-			<input
-				className="w-full px-3 py-2 border rounded"
-				type="text"
-				id="username"
-				name="username"
-				required
-				autoComplete="username"
-				value={username}
-				onChange={(e) => setUsername(e.target.value)}
-			/>
+				<label className="block mb-1 font-medium" htmlFor="username">
+					Username
+				</label>
+				<input
+					className="w-full px-3 py-2 border rounded"
+					type="text"
+					id="username"
+					name="username"
+					required
+					autoComplete="username"
+					value={username}
+					onChange={(e) => setUsername(e.target.value)}
+				/>
 			</div>
 			<div className="mb-6">
-			<label className="block mb-1 font-medium" htmlFor="password">
-				Password
-			</label>
-			<input
-				className="w-full px-3 py-2 border rounded"
-				type="password"
-				id="password"
-				name="password"
-				required
-				autoComplete="current-password"
-				value={password}
-				onChange={(e) => setPassword(e.target.value)}
-			/>
+				<label className="block mb-1 font-medium" htmlFor="password">
+					Password
+				</label>
+				<input
+					className="w-full px-3 py-2 border rounded"
+					type="password"
+					id="password"
+					name="password"
+					required
+					autoComplete="current-password"
+					value={password}
+					onChange={(e) => setPassword(e.target.value)}
+				/>
 			</div>
 			<div className="mb-6">
 				<Link
